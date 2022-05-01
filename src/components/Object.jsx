a7fa90a3d17be9210997aef445a47c6b779a7b54
Colleen Barbato <cawalizer@gmail.com>
2022-05-01 18:23:33 -0400

    script changes for distribution

--- src/components/Object.jsx
+++ src/components/Object.jsx
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import { useLoader , useFrame} from "@react-three/fiber";
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-//import * as THREE from "three";
 import katsura from "../assets/obj/katsura.obj";
 
 const Object = (props) => {
